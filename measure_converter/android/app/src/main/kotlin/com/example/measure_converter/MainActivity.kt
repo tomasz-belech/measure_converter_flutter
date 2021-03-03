ffb0186744942d4ea0de0aa6636d9979c886e5c5
Tomasz Belech <tomaszbelech1@gmail.com>
2021-03-03 19:26:57 +0100

    initial commit

---
+++ measure_converter/android/app/src/main/kotlin/com/example/measure_converter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.measure_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ measure_converter/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  double _numberFrom;
+
+    @override 
+  void initState() {
+  _numberFrom = 0;
+  super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Measures Converter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Measures Converter'),
+        ),
+        body: Center(
+          child: Column(children: [TextField(onChanged: (text) {
+            var rv = double.tryParse(text);
+            if (rv != null) {
+              setState(() {
+                _numberFrom = rv;
+              });
+            }
+          }), Text((_numberFrom == null) ? '' : _numberFrom.toString())],)
+        ),
+      ),
+    );
+  }
+}
